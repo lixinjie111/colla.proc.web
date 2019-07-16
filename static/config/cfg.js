50ab57418d6d24d197bd4143e9285c5b3ee0937c
shilu <shilu@tusvn.comm>
2019-07-16 10:34:55 +0800

    修改域名http://172.17.1.13, 改为http://120.133.21.14

--- static/config/cfg.js
+++ static/config/cfg.js
@@ -5,8 +5,11 @@ window.cfg = {
     // iconPath:'http://10.0.1.57:9090/file/pic/trafficType/',   
 
     //望京环境
-    url:'http://172.17.1.13:9094/collaProcApp/',                    //望京环境 
-    iconPath:'http://172.17.1.13:9090/file/pic/trafficType/',       // 图片服务器地址
+    // url:'http://172.17.1.13:9094/collaProcApp/',                    //望京环境 
+    // iconPath:'http://172.17.1.13:9090/file/pic/trafficType/',       // 图片服务器地址
+    //外网
+    url:'http://120.133.21.14:9094/collaProcApp/',                    //望京环境 
+    iconPath:'http://120.133.21.14:9090/file/pic/trafficType/',       // 图片服务器地址
     mapApiUrl: 'http://113.208.118.62:8085/',                        // 地图api
     // mapApiUrl: 'http://120.133.21.14:8085/',                        // 地图api
 
