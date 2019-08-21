f5929029f5fca7d18a0e52f619bb653b5122e7ad
shaoxiaoli <shaoxiaoli@tusvn.com>
2019-08-21 18:52:36 +0800

    记住密码功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -19,6 +19,7 @@
               v-model.trim="loginForm.userNo"
               :maxlength="40"
               placeholder="请输入用户名"
+              auto-complete="new-text"
             ></el-input>
           </el-form-item>
           <el-form-item prop="password" label="密码：" class="login-item">
@@ -27,6 +28,7 @@
               v-model.trim="loginForm.password"
               :maxlength="20"
               placeholder="请输入密码"
+              auto-complete="new-password"
             ></el-input>
           </el-form-item>
           <el-form-item class="login-item login-remember-item">
@@ -79,11 +81,19 @@ export default {
       loading: false
     };
   },
+  mounted() {
+    this.getCookie();
+  },
   methods: {
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
           this.loading = true;
+          if (this.checked === true) {
+            this.setCookie(this.loginForm.userNo, this.loginForm.password, 7);
+          } else {
+            this.clearCookie();
+          }
           this.$api.post(
             "sys/user/login",
             {
@@ -100,6 +110,7 @@ export default {
                   this.loading = false;
                   let temp = response.data.data;
                   temp = JSON.parse(temp);
+                  console.log("temp", temp);
                   this.$store.dispatch("login", temp);
                   LocalStorageUtil.setItem("login", temp);
                   LocalStorageUtil.setItem("currentMenu", "/infoRelease");
@@ -121,9 +132,50 @@ export default {
           return false;
         }
       });
+    },
+    //设置cookie
+    setCookie(c_name, c_pwd, exdays) {
+      var exdate = new Date(); //获取时间
+      exdate.setTime(exdate.getTime() + 24 * 60 * 60 * 1000 * exdays); //保存的天数
+      //字符串拼接cookie
+      window.document.cookie =
+        "rememberUserName" +
+        "=" +
+        c_name +
+        ";path=/;expires=" +
+        exdate.toGMTString();
+      window.document.cookie =
+        "rememberUserPwd" +
+        "=" +
+        c_pwd +
+        ";path=/;expires=" +
+        exdate.toGMTString();
+    },
+    //读取cookie
+    getCookie: function() {
+      if (document.cookie.length > 0) {
+        var arr = document.cookie.split("; "); //这里显示的格式需要切割一下自己可输出看下
+        for (var i = 0; i < arr.length; i++) {
+          var arr2 = arr[i].split("="); //再次切割
+          //判断查找相对应的值
+          if (arr2[0] == "rememberUserName") {
+            this.loginForm.userNo = arr2[1]; //保存到保存数据的地方
+          } else if (arr2[0] == "rememberUserPwd") {
+            this.loginForm.password = arr2[1];
+          }
+        }
+      } else {
+        this.$refs.loginForm.resetFields();
+        this.loginForm.userNo = "";
+        this.loginForm.password = "";
+        this.checked = false;
+      }
+    },
+    //清除cookie
+    clearCookie: function() {
+      this.setCookie("", "", -1); //修改2值都为空，天数为负1天就好了
     }
   },
-  mounted() {},
   destroyed() {}
 };
 </script>
