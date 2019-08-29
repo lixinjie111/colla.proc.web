5e76253619aa36ab9b4550c4a81505be71c41b2b
shilu <shilu@tusvn.comm>
2019-08-29 13:57:12 +0800

    长沙分支修改IP地址

--- static/config/config.js
+++ static/config/config.js
@@ -1,28 +1,16 @@
-window.config = {    
-    
-    // 本地测试环境
-    // url:'http://10.0.1.57:9094/collaProcApp/',   
-    // iconPath:'http://10.0.1.57:9090/file/pic/trafficType/',   
+window.config = {   
+ 
+    // 172.17.1.13
+    // 10.0.1.22
 
-    //望京环境
-    // url:'http://172.17.1.13:9094/collaProcApp/',                    //望京环境 
-    // iconPath:'http://172.17.1.13:9090/file/pic/trafficType/',       // 图片服务器地址
-    // mapApiUrl: 'http://120.133.21.14:8085/',                        // 地图api
-    // mapApiUrl: 'http://192.168.0.1:8085/', 
-    // mapApiUrl: 'http://113.208.118.62:8085/',                        // 地图api
+    // 120.133.21.14
+    // 113.208.118.62
 
-    //内网
-    // url:'http://172.17.1.13:9094/collaProcApp/',                    //望京环境 
-    // iconPath:'http://172.17.1.13:9090/file/pic/trafficType/',       // 图片服务器地址
-    // mapWaPianPath:'http://172.17.1.13:8086/',   //地图瓦片
-    // dlUrl: 'http://10.0.1.22:8085/', //迪路
-    // dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
-    //外网
-    url:'http://120.133.21.14:9094/collaProcApp/',                    //望京环境 
-    iconPath:'http://120.133.21.14:9090/file/pic/trafficType/',       // 图片服务器地址
-    mapWaPianPath:'http://120.133.21.14:8086/',   //地图瓦片
-    dlUrl: 'http://113.208.118.62:8085/', //迪路
-    dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
+    url:'http://111.23.140.59:9094/collaProcApp/',                    //望京环境 
+    iconPath:'http://111.23.140.59:9090/file/pic/trafficType/',       // 图片服务器地址
+    mapWaPianPath:'http://111.23.140.59:8086/',   //地图瓦片
+    dlUrl: 'http://111.23.140.59:8085/', //迪路
+    dlWmsUrl: 'http://111.23.140.59:8080/', //迪路
 
     version: 1.0,       // 版本号
 }
