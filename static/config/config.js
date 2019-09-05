e7c6a0e428cb50d5a38e82013dccdb3e5397d695
shaoxiaoli <shaoxiaoli@tusvn.com>
2019-09-05 14:38:34 +0800

    修改config文件地址

--- static/config/config.js
+++ static/config/config.js
@@ -1,19 +1,27 @@
 window.config = {    
-    // 公司测试 外网地址
-    url:'http://120.133.21.14:9094/collaProcApp/', // 交通平台
-    operateUrl: 'http://120.133.21.14:9090/operateApp/',	//运营平台
+    //公司测试 外网地址
+    url:'http://120.133.21.14:9094/collaProcApp/',  // 交通平台
+    operateUrl: 'http://120.133.21.14:9090/operateApp/',	//运营平台登陆
     iconPath:'http://120.133.21.14:9090/file/pic/trafficType/',  // 图片服务器地址
     mapWaPianPath:'http://120.133.21.14:8086/',   //地图瓦片
-    dlUrl: 'http://113.208.118.62:8085/', //迪路
-    dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
-    
-    // 上海  内网
+    dlUrl: 'http://120.133.21.14:8085/', //迪路
+    dlWmsUrl: 'http://120.133.21.14:8080/', //迪路
+
+    //上海  内网-IP
     // url:'http://172.16.1.197:9094/collaProcApp/',  // 交通平台
-    // operateUrl: 'http://172.16.1.197:9090/operateApp/',	//运营平台登陆
+    // operateUrl: 'http://172.16.1.197:9090/operateApp/', //运营平台登陆
     // iconPath:'http://172.16.1.197:9090/file/pic/trafficType/',  // 图片服务器地址
     // mapWaPianPath:'http://172.16.1.197:8086/',   //地图瓦片
     // dlUrl: 'http://172.16.1.197:8085/', //迪路
     // dlWmsUrl: 'http://172.16.1.197:8080/', //迪路
+
+    // //上海  内网-主机名
+    // url:'http://app01:9094/collaProcApp/',  // 交通平台
+    // operateUrl: 'http://app01:9090/operateApp/', //运营平台登陆
+    // iconPath:'http://app01:9090/file/pic/trafficType/',  // 图片服务器地址
+    // mapWaPianPath:'http://app01:8086/',   //地图瓦片
+    // dlUrl: 'http://app01:8085/', //迪路
+    // dlWmsUrl: 'http://app01:8080/', //迪路
 
     version: 1.0,       // 版本号
 }
