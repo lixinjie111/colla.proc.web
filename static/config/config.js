41d8c9bb4f5205ce2555406cc4b712afbad8448c
shilu <shilu@tusvn.comm>
2019-09-10 10:23:05 +0800

    config添加正式环境访问地址

--- static/config/config.js
+++ static/config/config.js
@@ -1,17 +1,6 @@
-window.config = {    
-    
-    // 本地测试环境
-    // url:'http://10.0.1.57:9094/collaProcApp/',   
-    // iconPath:'http://10.0.1.57:9090/file/pic/trafficType/',   
+window.config = { 
 
-    //望京环境
-    // url:'http://172.17.1.13:9094/collaProcApp/',                    //望京环境 
-    // iconPath:'http://172.17.1.13:9090/file/pic/trafficType/',       // 图片服务器地址
-    // mapApiUrl: 'http://120.133.21.14:8085/',                        // 地图api
-    // mapApiUrl: 'http://192.168.0.1:8085/', 
-    // mapApiUrl: 'http://113.208.118.62:8085/',                        // 地图api
-
-    //内网
+    //望京 内网
     // url:'http://172.17.1.16:9094/collaProcApp/',                    //望京环境 
     // operateUrl: 'http://172.17.1.16:9090/operateApp/',	//运营平台
     // iconPath:'http://172.17.1.16:9090/file/pic/trafficType/',       // 图片服务器地址
@@ -19,13 +8,21 @@ window.config = {
     // dlUrl: 'http://10.0.1.22:8085/', //迪路
     // dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
     
-    //外网
+    //望京 外网
     url:'http://120.133.21.14:9094/collaProcApp/',                  //望京环境 
     operateUrl: 'http://120.133.21.14:9090/operateApp/',	//运营平台
     iconPath:'http://120.133.21.14:9090/file/pic/trafficType/',       // 图片服务器地址
     mapWaPianPath:'http://120.133.21.14:8086/',   //地图瓦片
     dlUrl: 'http://113.208.118.62:8085/', //迪路
     dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
+
+    //上海正式环境 内网
+    // url:'http://172.16.1.197:9094/collaProcApp/',  // 交通平台
+    // operateUrl: 'http://172.16.1.197:9090/operateApp/', //运营平台登陆
+    // iconPath:'http://172.16.1.197:9090/file/pic/trafficType/',  // 图片服务器地址
+    // mapWaPianPath:'http://172.16.1.197:8086/',   //地图瓦片
+    // dlUrl: 'http://172.16.1.197:8085/', //迪路
+    // dlWmsUrl: 'http://172.16.1.197:8080/', //迪路
 
     version: 1.0,       // 版本号
 }
