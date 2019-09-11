4a8ffd8f8e83561faf57c406b41e3de965d10ed9
shilu <shilu@tusvn.comm>
2019-09-11 11:46:19 +0800

    优化config

--- static/config/config.js
+++ static/config/config.js
@@ -22,6 +22,7 @@ window.config = {
     // operateUrl: 'http://172.16.1.197:9090/operateApp/', //运营平台登陆
     // iconPath:'http://172.16.1.197:9090/file/pic/trafficType/',  // 图片服务器地址
     // mapWaPianPath:'http://172.16.1.197:8086/',   //地图瓦片
+    // websocketUrl:'ws://172.16.1.197:9996/colla',
     // dlUrl: 'http://172.16.1.197:8085/', //迪路
     // dlWmsUrl: 'http://172.16.1.197:8080/', //迪路
 
@@ -30,8 +31,18 @@ window.config = {
     // operateUrl: 'http://app01:9090/operateApp/', //运营平台登陆
     // iconPath:'http://app01:9090/file/pic/trafficType/',  // 图片服务器地址
     // mapWaPianPath:'http://app01:8086/',   //地图瓦片
+    // websocketUrl:'ws://app01:9996/colla',
     // dlUrl: 'http://app01:8085/', //迪路
     // dlWmsUrl: 'http://app01:8080/', //迪路
+
+    //上海正式环境 外网
+    // url:'http://116.236.72.204:9094/collaProcApp/',  // 交通平台
+    // operateUrl: 'http://116.236.72.204:9090/operateApp/', //运营平台登陆
+    // iconPath:'http://116.236.72.204:9090/file/pic/trafficType/',  // 图片服务器地址
+    // mapWaPianPath:'http://116.236.72.204:8086/',   //地图瓦片
+    // websocketUrl:'ws://116.236.72.204:9996/colla',
+    // dlUrl: 'http://116.236.72.204:8085/', //迪路
+    // dlWmsUrl: 'http://116.236.72.204:8080/', //迪路
 
     version: 1.0,       // 版本号
 }
