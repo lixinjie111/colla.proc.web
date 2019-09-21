ba3f4905d4f63180168436332298654d7962dfa1
shilu <shilu@tusvn.comm>
2019-09-21 18:00:39 +0800

    地址配置

--- static/config/config.js
+++ static/config/config.js
@@ -1,27 +1,19 @@
 window.config = {  
-     //内网
-    // url:'http://172.17.1.16:9094/collaProcApp/',                    //望京环境 
-    // operateUrl: 'http://172.17.1.16:9090/operateApp/',	//运营平台
-    // iconPath:'http://172.17.1.16:9090/file/pic/trafficType/',       // 图片服务器地址
-    // mapWaPianPath:'http://172.17.1.134:8086/',   //地图瓦片
-    // websocketUrl:'ws://172.17.1.16:9996/colla',
-    // dlUrl: 'http://10.0.1.22:8085/', //迪路
-    // dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
     
-    //外网
-    url:'http://120.133.21.14:9094/collaProcApp/',                  //望京环境 
-    operateUrl: 'http://120.133.21.14:9090/operateApp/',	//运营平台
-    iconPath:'http://120.133.21.14:9090/file/pic/trafficType/',       // 图片服务器地址
-    mapWaPianPath:'http://120.133.21.14:8086/',   //地图瓦片
-    websocketUrl:'ws://120.133.21.14:49996/colla', 
-    dlUrl: 'http://113.208.118.62:8085/', //迪路
-    dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
+    //雄安 外网地址
+    url:'http://144.7.122.84:9094/collaProcApp/',                  //望京环境 
+    operateUrl: 'http://144.7.122.84:9090/operateApp/',	//运营平台
+    iconPath:'http://144.7.122.84:9090/file/pic/trafficType/',       // 图片服务器地址
+    websocketUrl:'ws://144.7.122.84:9996/colla', 
+    mapWaPianPath:'http://144.7.122.89:8086/',   //地图瓦片
+    dlUrl: 'http://144.7.122.89:8085/', //迪路
+    dlWmsUrl: 'http://144.7.122.89:8080/', //迪路
 
     version: 1.0,       // 版本号
 }
 //地图额外配置项添加
 window.defaultMapOption = {
-    center: [121.1882114,31.2771320], // 上海 -- gps坐标点
+    center: [115.912987,39.049487], // 雄安雄安市民服务中心 -- gps坐标点
     zoom: 14
 }
 //路网配置参数
