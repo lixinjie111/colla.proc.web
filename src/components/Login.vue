83a227760a363b24aaddb2d36db7f8ad19f1b114
shilu <shilu@tusvn.comm>
2019-09-23 14:33:24 +0800

    还原

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -86,25 +86,24 @@ export default {
     };
   },
   created() {
-      this.visibleFlag = true;
-      // let _data = localStorage.getItem("yk-token");
-      // if(_data) {
-      //     let _dataObj = JSON.parse(_data),
-      //         _delayTime = 1000 * 60 * 60 * 24;
-      //     if (new Date().getTime() - _dataObj.time > _delayTime) {
-      //         console.log('信息已过期');
-      //         this.removeStorage();
-      //     }else{
-      //         // 直接调用登录接口
-      //         let _params = {
-      //             token: _dataObj.data,
-      //             platform: '40000'
-      //         };
-      //         this.loginFunc(_params);
-      //     }
-      // } else {
-      //     this.removeStorage();
-      // }
+      let _data = localStorage.getItem("yk-token");
+      if(_data) {
+          let _dataObj = JSON.parse(_data),
+              _delayTime = 1000 * 60 * 60 * 24;
+          if (new Date().getTime() - _dataObj.time > _delayTime) {
+              console.log('信息已过期');
+              this.removeStorage();
+          }else{
+              // 直接调用登录接口
+              let _params = {
+                  token: _dataObj.data,
+                  platform: '40000'
+              };
+              this.loginFunc(_params);
+          }
+      } else {
+          this.removeStorage();
+      }
   },
   mounted() {
     this.getCookie();
@@ -113,22 +112,13 @@ export default {
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
-          if(this.loginForm.userNo == 'qcctest1') {
-            this.loading = true;
-            if (this.checked === true) {
-              this.setCookie(this.loginForm.userNo, this.loginForm.password, 7);
-            } else {
-              this.clearCookie();
-            }
-            this.loginFunc(this.loginForm);
-          }else {
-            this.$message({
-                type: 'error',
-                duration: '1500',
-                message: '该用户没有权限，请重试！',
-                showClose: true
-            });
+          this.loading = true;
+          if (this.checked === true) {
+            this.setCookie(this.loginForm.userNo, this.loginForm.password, 7);
+          } else {
+            this.clearCookie();
           }
+          this.loginFunc(this.loginForm);
         } else {
           this.loading = false;
         }
