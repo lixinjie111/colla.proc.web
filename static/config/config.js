7e9baef0fa193834be57b879b3ac7b3d38f5f4a2
shilu <shilu@tusvn.comm>
2019-12-03 18:50:51 +0800

    测试地址管理

--- static/config/config.js
+++ static/config/config.js
@@ -18,6 +18,16 @@ window.config = {
     websocketUrl:'ws://120.133.21.14:49996/colla', 
     dlUrl: 'http://117.114.144.227:8085/', //迪路
     dlWmsUrl: 'http://117.114.144.227:8080/', //迪路
+    
+    //测试环境 内网
+    // staticUrl: "http://10.0.1.181:9090/", //静态文件路径
+    // url:'http://10.0.1.181:9094/collaProcApp/',                  //望京环境 
+    // operateUrl: 'http://10.0.1.181:9090/operateApp/',    //运营平台
+    // iconPath:'http://10.0.1.181:9090/file/pic/trafficType/',       // 图片服务器地址
+    // mapWaPianPath:'http://10.0.1.181:8086/',   //地图瓦片
+    // websocketUrl:'ws://10.0.1.181:49996/colla', 
+    // dlUrl: 'http://10.0.1.199:8085/', //迪路
+    // dlWmsUrl: 'http://10.0.1.199:8080/', //迪路
 
     //上海正式环境 外网
     // staticUrl: "http://116.236.72.204:49090/", //静态文件路径
