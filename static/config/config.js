afce3bf9799efc09e204c139cb7638b39b252c31
shilu <shilu@tusvn.comm>
2019-12-27 11:00:32 +0800

    静态文件目录地址改为外链

--- static/config/config.js
+++ static/config/config.js
@@ -10,14 +10,14 @@ window.config = {
     // dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
     
     //望京 外网
-    staticUrl: "http://120.133.21.14:9090/", //静态文件路径
-    url:'http://120.133.21.14:9094/collaProcApp/',                  //望京环境 
-    operateUrl: 'http://120.133.21.14:9090/operateApp/',    //运营平台
-    iconPath:'http://120.133.21.14:9090/file/pic/trafficType/',       // 图片服务器地址
-    mapWaPianPath:'http://120.133.21.14:8086/',   //地图瓦片
-    websocketUrl:'ws://120.133.21.14:49996/colla', 
-    dlUrl: 'http://117.114.144.227:8085/', //迪路
-    dlWmsUrl: 'http://117.114.144.227:8080/', //迪路
+    // staticUrl: "http://120.133.21.14:9090/", //静态文件路径
+    // url:'http://120.133.21.14:9094/collaProcApp/',                  //望京环境 
+    // operateUrl: 'http://120.133.21.14:9090/operateApp/',    //运营平台
+    // iconPath:'http://120.133.21.14:9090/file/pic/trafficType/',       // 图片服务器地址
+    // mapWaPianPath:'http://120.133.21.14:8086/',   //地图瓦片
+    // websocketUrl:'ws://120.133.21.14:49996/colla', 
+    // dlUrl: 'http://117.114.144.227:8085/', //迪路
+    // dlWmsUrl: 'http://117.114.144.227:8080/', //迪路
     
     //测试环境 内网
     // staticUrl: "http://10.0.1.181:9090/", //静态文件路径
@@ -30,14 +30,14 @@ window.config = {
     // dlWmsUrl: 'http://10.0.1.199:8080/', //迪路
 
     //上海正式环境 外网
-    // staticUrl: "http://116.236.72.204:49090/", //静态文件路径
-    // url:'http://116.236.72.204:49094/collaProcApp/',  // 交通平台
-    // operateUrl: 'http://116.236.72.204:49090/operateApp/', //运营平台登陆
-    // iconPath:'http://116.236.72.204:49090/file/pic/trafficType/',  // 图片服务器地址
-    // mapWaPianPath:'http://116.236.72.204:48086/',   //地图瓦片
-    // websocketUrl:'ws://116.236.72.204:49996/colla',
-    // dlUrl: 'http://116.236.72.204:48085/', //迪路
-    // dlWmsUrl: 'http://116.236.72.204:48080/', //迪路
+    staticUrl: "http://116.236.72.204:49090/", //静态文件路径
+    url:'http://116.236.72.204:49094/collaProcApp/',  // 交通平台
+    operateUrl: 'http://116.236.72.204:49090/operateApp/', //运营平台登陆
+    iconPath:'http://116.236.72.204:49090/file/pic/trafficType/',  // 图片服务器地址
+    mapWaPianPath:'http://116.236.72.204:48086/',   //地图瓦片
+    websocketUrl:'ws://116.236.72.204:49996/colla',
+    dlUrl: 'http://116.236.72.204:48085/', //迪路
+    dlWmsUrl: 'http://116.236.72.204:48080/', //迪路
 
     version: "1.2",       // 版本号
 }
