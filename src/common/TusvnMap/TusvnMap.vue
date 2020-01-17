2979d681d2677303af6ea43bc17327b7d95175df
lixinjie <15110074982@139.com>
2020-01-17 15:11:08 +0800

    优化

--- src/common/TusvnMap/TusvnMap.vue
+++ src/common/TusvnMap/TusvnMap.vue
@@ -682,6 +682,7 @@ export default {
                 "eventCode": this.trafficInfo.eventCode,
                 "alertCategory": this.trafficInfo.alertCategory,
                 "alertRadius": this.trafficInfo.alertRadius,
+                "isSend":true,
                 "expirationTime": new Date().getTime(), 
                 "status": 2                
             };
